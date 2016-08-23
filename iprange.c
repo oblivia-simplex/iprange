7dd86401c4900e9602fd6a189ef4dd0eb3fb0eff
Olivia Lucca Fraser <oluccafraser@tenable.com>
2016-08-23 18:37:50 -0300

    First commit

---
+++ iprange.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include "iprange.h"
+
+unsigned int ip2int (char *ipstr){
+  unsigned char o0, o1, o2, o3;
+  unsigned int ipint;
+  sscanf(ipstr, "%hd.%hd.%hd.%hd", &o3, &o2, &o1, &o0);
+  ipint = (o0) | (o1 << 8) | (o2 << 16) | (o3 << 24);
+  return ipint;
+}
+
+void int2ip (unsigned int ipint, char *ipstr) {
+  unsigned char o[4];
+  int i;
+  memset(ipstr, 0, 18);
+  for (i = 0; i < 4; i++){
+    o[i] = ipint & 0xFF;
+      ipint >>= 8;
+  }
+  snprintf(ipstr, 18, "%u.%u.%u.%u", o[3], o[2], o[1], o[0]);
+}
+  
+
+void cidr_min_max(char *cidr, unsigned int *min, unsigned int *max){
+  unsigned char mask, offset;
+  int ipint, bitmask, sep = 0;
+  char ipstr[18];
+  memset(ipstr, 0, 18);
+
+  while (cidr[++sep] != '/')
+    ;
+
+  sscanf(cidr+sep, "/%d", &mask);
+  sscanf(cidr, "%s", ipstr);
+  ipint = ip2int(ipstr);
+  offset = 32 - mask;
+  bitmask = (0xFFFFFFFF >> offset) << offset;
+  *min = ipint & bitmask;
+  *max = ipint | (~bitmask);
+}
+
+void cidr_list(char *cidr){
+  unsigned int min, max;
+  cidr_min_max(cidr, &min, &max);
+  list_min_max(min, max);
+  return;
+}
+
+void list_min_max(unsigned int min, unsigned int max){
+  char ipstr[18];
+  while (min <= max) {
+    int2ip(min++, ipstr);
+    printf("%s\n", ipstr);
+  }
+  return;
+}
+
+int main (int argc, char **argv){
+
+  if (argc == 2){
+    cidr_list (argv[1]);
+    return 0;
+  } else if (argc == 3) {
+    unsigned int min, max;
+    min = ip2int(argv[1]);
+    max = ip2int(argv[2]);
+    list_min_max(min, max);
+    return 0;
+  } else {
+    printf("Usage: %s <min-ip> <max-ip>\n           -- or --\n       %s <cidr>\n", argv[0], argv[0]);
+    return (1);
+  }
+
+}
---
+++ iprange.h
@@ -0,0 +1,6 @@
+unsigned int ip2int (char *ipstr);
+void int2ip (unsigned int ipint, char *ipstr);
+void cidr_min_max(char *cidr, unsigned int *min, unsigned int *max);
+void cidr_list(char *cidr);
+void list_min_max(unsigned int min, unsigned int max);
+
